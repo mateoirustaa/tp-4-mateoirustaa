71b0c43a7e97390146f95e2dbc8736fcfe917c48
mateoirustaa <162723360+mateoirustaa@users.noreply.github.com>
2024-04-23 12:21:16 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    año=int(input("Ingrese un año:  "))
+if (año%100)==0 and(año%400)==0:
+        print("el año es biciesto")
+elif (año%4)==0 and (año%100!=0):
+    print("el año es biciesto2")
+else :
+    print("el año no es biciesto")
